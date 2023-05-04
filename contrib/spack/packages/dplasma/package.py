510f88d84307ffebb8a417f5b6bd5ca40fc1110e
Omri Mor <omrimor2@illinois.edu>
2023-05-04 13:58:32 -0700

    spack: new precisions variant, misc changes

--- contrib/spack/packages/dplasma/package.py
+++ contrib/spack/packages/dplasma/package.py
@@ -7,6 +7,7 @@
 #
 ##############################################################################
 from spack import *
+import os
 
 class Dplasma(CMakePackage):
     """DPLASMA: The distributed, accelerator enabled, dense linear algebra library"""
@@ -20,14 +21,25 @@ class Dplasma(CMakePackage):
     version('2.0.0', '')
 
     variant('install_tests', default=False, description='Install tests')
+    variant('precisions', default='s,d,c,z', values=('s', 'd', 'c', 'z'),
+            multi=True, description='Floating point precisions to build')
+
+    generator = 'Ninja'
+    depends_on('ninja', type='build')
 
     depends_on('cmake@3.16.0:', type='build')
     depends_on('parsec')
+    depends_on('blas')
     depends_on('lapack')
+
+    @property
+    def build_directory(self):
+        return os.path.join(self.stage.path, 'spack-build', self.spec.dag_hash(7))
 
     def cmake_args(self):
         args = [
             self.define_from_variant('DPLASMA_INSTALL_TESTS', 'install_tests'),
+            self.define_from_variant('DPLASMA_PRECISIONS', 'precisions'),
         ]
         args.extend(self.spec['lapack'].cmake_args)
         return args
